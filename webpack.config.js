b2a13053533f0652931ae97daa990d9bfba8614e
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-13 02:31:09 -0400

    Refacttor app.jsx into separate components, configure webpack config to provide aliases

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/**
+ * Created by manishgiri on 3/12/17.
+ */
+
+/*module.exports = {
+    entry: './public/app.js',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '']
+    }
+}; */
+
+module.exports = {
+    entry: './public/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+          Greeter: 'public/components/Greeter.jsx',
+            GreeterMessage: 'public/components/GreeterMessage.jsx',
+            GreeterForm: 'public/components/GreeterForm.jsx'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+};
+
